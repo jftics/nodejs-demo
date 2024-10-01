988166e89c985bc188cb61a493c436b2c444b777
jftics <jfmatics@gmail.com>
2024-10-01 18:29:15 -0400

    node demostracion

---
+++ config.js
@@ -0,0 +1,23 @@
+export const configDB = {
+
+    user: 'jftics_dbpg_demo_vnwu_user',//'user_node',
+    password:'EtB7S1XCmp0GlCfZWrl6ptLjCgspwYp3',//'123456',
+    database: 'jftics_dbpg_demo_vnwu',//'db_node',
+    host: 'dpg-cru79trv2p9s73en588g-a.oregon-postgres.render.com',//'localhost',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+
+    ssl: true
+
+}
+
+export const configHash = {
+    SALT_HASH:10
+}
+
+export const  configToken = {
+    SECRET_ACCESS_TOKEN : 'UNACADENABASTANTELARGA',
+    SECRET_REFRESH_TOKEN : 'UNACADENABASTANTELARGAPRAACTUALIZACION'
+}
